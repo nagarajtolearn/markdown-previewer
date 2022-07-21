2908a694ec7dee9445bf9ab7f441a87f2abb2bff
nagarajtolearn <nagarajtolearn@gmail.com>
2022-07-21 18:21:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
 function App() {
+  const [text, setText] = useState(defaultMarkdown);
+  const handleChange = (e) => setText(e.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">Markdown Previewer</h1>
+      <textarea
+        name="editor"
+        id="editor"
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter markdown..."
+      />
+
+      <div id="preview">
+        <ReactMarkdown children={text} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const defaultMarkdown = `# React Markdown Previewer
+
+## Type your Markdown in the Editor!
+&nbsp;&nbsp;
+
+### Main functionality
+
+- Preview window updates real time with markdown syntax
+- The editor has some predefined input on page load
+- BONUS: Use &lt;br&gt; for line breaks
+
+&nbsp;
+
+\`Is the syntax highlighting even working?\`
+\`\`\`javascript
+let s = "JavaScript syntax highlighting";
+alert(s);
+\`\`\`
+&nbsp;
+
+> “The mind is everything. What you think you become.”
+― Buddha
+&nbsp;
+
+![react logo](https://i.postimg.cc/Bv9y8sBZ/react-logo.png)
+&nbsp;
+
+Coded by **Nagaraj Biradar**, 2022`;
